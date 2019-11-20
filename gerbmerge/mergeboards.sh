7e3a46ad140170be3edc70684b4ef05dec725ebc
jbenoit <jbenoit@t-online.de>
2019-11-20 13:48:58 +0100

    change layout and project setup

--- gerbmerge/mergeboards.sh
+++ gerbmerge/mergeboards.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gerbmerge --search-timeout 3 --no-trim-gerber gerbmerge_atmwm.cfg
+gerbmerge --search-timeout 3 --no-trim-gerber gerbmerge_atmwm.cfg gerbmerge_atmwm.def
 rm merged.drills-unplated.xln 
 
 
