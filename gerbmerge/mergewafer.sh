bb2e54a8a3adbac2bca323dd423efb1a779c7d6b
jbenoit <jbenoit@t-online.de>
2020-10-10 17:07:52 +0200

    adapt gerbmerge for py3

--- gerbmerge/mergewafer.sh
+++ gerbmerge/mergewafer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gerbmerge --search-timeout 3 --no-trim-gerber gerbmerge_wafer.cfg
+gerbmerge --ack --search-timeout 3 --no-trim-gerber gerbmerge_wafer.cfg gerbmerge_wafer.def
 rm merged.drills-unplated.xln 
 
 
