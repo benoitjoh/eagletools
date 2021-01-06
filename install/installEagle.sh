f2e6c0cec2d22de125d9543b3d8bb5421c2c317d
jbenoit <jbenoit@t-online.de>
2021-01-06 19:26:16 +0100

    installscript for missing library for eagle

---
+++ install/installEagle.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+# install old library for eagle 7
+sudo dpkg -i libssl1.0.0_1.0.2g-1ubuntu4.18_amd64.deb
+
+echo "Download installpackage from http://eagle.autodesk.com/eagle/software-versions/1"
+
