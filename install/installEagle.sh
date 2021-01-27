e7607a2303d8cdb40ada886f3dfbdc30b247a659
Johannes Benoit <jbenoit@t-online.de>
2021-01-27 09:39:38 +0100

    try to automatic download installscript for eagle

--- install/installEagle.sh
+++ install/installEagle.sh
@@ -2,5 +2,10 @@
 # install old library for eagle 7
 sudo dpkg -i libssl1.0.0_1.0.2g-1ubuntu4.18_amd64.deb
 
-echo "Download installpackage from http://eagle.autodesk.com/eagle/software-versions/1"
+echo "trying to downloade eagle from http://eagle.autodesk.com/eagle/software-versions/1"
+wget http://eagle.autodesk.com/eagle/download-software/5
+mv 5 eaglesetup770.run
+chmod 755 eaglesetup770.run
 
+#firefox http://eagle.autodesk.com/eagle/software-versions/1
+./eaglesetup770.run
